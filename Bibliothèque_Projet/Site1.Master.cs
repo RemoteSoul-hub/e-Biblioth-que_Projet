fafcc84f13464725e10fbbc9434fbbdf5383bcfc
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-25 22:44:41 +0000

    Master page buttons onclick

--- Bibliothèque_Projet/Site1.Master.cs
+++ Bibliothèque_Projet/Site1.Master.cs
@@ -13,5 +13,55 @@ namespace Bibliothèque_Projet
         {
 
         }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminlogin.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminauthormanagement.aspx");
+        }
+
+        protected void LinkButton5_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("admincategorymanagement.aspx");
+        }
+
+        protected void LinkButton8_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("bookinventory.aspx");
+        }
+
+        protected void LinkButton9_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("bookstatuspage.aspx");
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("viewbooks.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("usersignup.aspx");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("userlogin.aspx");
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
