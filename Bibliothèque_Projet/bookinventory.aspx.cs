34f9b1168d6e9da6b7d677429f06e5320ee43a1b
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-26 18:29:32 +0000

    Update / Delete Book Details Added

--- Bibliothèque_Projet/bookinventory.aspx.cs
+++ Bibliothèque_Projet/bookinventory.aspx.cs
@@ -14,6 +14,9 @@ namespace Bibliothèque_Projet
     public partial class bookinventory : System.Web.UI.Page
     {
         string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        static string global_filepath;
+        static int global_updated_stock, global_current_stock, global_used_books;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             fillAuthorCategoryValues();
@@ -23,19 +26,19 @@ namespace Bibliothèque_Projet
         // go button click
         protected void LinkButton4_Click(object sender, EventArgs e)
         {
-          
+            getBookByID();
         }
 
 
         // update button click
         protected void Button3_Click(object sender, EventArgs e)
         {
-           
+            updateBookByID();
         }
         // delete button click
         protected void Button2_Click(object sender, EventArgs e)
         {
-           
+            deleteBookByID();
         }
         // add button click
         protected void Button1_Click(object sender, EventArgs e)
@@ -50,6 +53,165 @@ namespace Bibliothèque_Projet
             }
         }
         // fonctions d'utilisation
+        // Trouver livre avec ID
+        void getBookByID()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * from book_master_tbl WHERE book_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox2.Text = dt.Rows[0]["book_name"].ToString();
+                    TextBox3.Text = dt.Rows[0]["publish_date"].ToString();
+                    TextBox9.Text = dt.Rows[0]["edition"].ToString();
+                    TextBox10.Text = dt.Rows[0]["ISBN"].ToString().Trim();
+                    TextBox4.Text = dt.Rows[0]["updated_stock"].ToString().Trim();
+                    TextBox5.Text = dt.Rows[0]["current_stock"].ToString().Trim();
+                    TextBox6.Text = dt.Rows[0]["description"].ToString();
+                    TextBox7.Text = "" + (Convert.ToInt32(dt.Rows[0]["updated_stock"].ToString()) - Convert.ToInt32(dt.Rows[0]["current_stock"].ToString()));
+
+                    DropDownList2.SelectedValue = dt.Rows[0]["language"].ToString().Trim();
+                    DropDownList3.SelectedValue = dt.Rows[0]["author_name"].ToString().Trim();
+
+                    global_updated_stock = Convert.ToInt32(dt.Rows[0]["updated_stock"].ToString().Trim());
+                    global_current_stock = Convert.ToInt32(dt.Rows[0]["current_stock"].ToString().Trim());
+                    global_used_books = global_updated_stock - global_current_stock;
+                    global_filepath = dt.Rows[0]["book_img_link"].ToString();
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Book ID');</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        void updateBookByID()
+        {
+
+            if (checkIfBookExists())
+            {
+                try
+                {
+
+                    int updated_stock = Convert.ToInt32(TextBox4.Text.Trim());
+                    int current_stock = Convert.ToInt32(TextBox5.Text.Trim());
+
+                    if (global_updated_stock == updated_stock)
+                    {
+
+                    }
+                    else
+                    {
+                        if (updated_stock < global_used_books)
+                        {
+                            Response.Write("<script>alert('Actual Stock value cannot be less than the Issued books');</script>");
+                            return;
+
+
+                        }
+                        else
+                        {
+                            current_stock = updated_stock - global_used_books;
+                            TextBox5.Text = "" + current_stock;
+                        }
+                    }
+
+                    string filepath = "~/inventaire_livres/books1";
+                    string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    if (filename == "" || filename == null)
+                    {
+                        filepath = global_filepath;
+
+                    }
+                    else
+                    {
+                        FileUpload1.SaveAs(Server.MapPath("inventaire_livres/" + filename));
+                        filepath = "~/inventaire_livres/" + filename;
+                    }
+
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    SqlCommand cmd = new SqlCommand("UPDATE book_master_tbl set book_name=@book_name, category=@category, author_name=@author_name, publish_date=@publish_date, language=@language, edition=@edition, ISBN=@ISBN, description=@description, updated_stock=@updated_stock, current_stock=@current_stock, book_img_link=@book_img_link where book_id='" + TextBox1.Text.Trim() + "'", con);
+
+                    cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+                    cmd.Parameters.AddWithValue("@book_name", TextBox2.Text.Trim());
+                    cmd.Parameters.AddWithValue("@category", DropDownList4.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@author_name", DropDownList3.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@publish_date", TextBox3.Text.Trim());
+                    cmd.Parameters.AddWithValue("@language", DropDownList2.SelectedItem.Value);
+                    cmd.Parameters.AddWithValue("@edition", TextBox9.Text.Trim());
+                    cmd.Parameters.AddWithValue("@ISBN", TextBox10.Text.Trim());
+                    cmd.Parameters.AddWithValue("@updated_stock", TextBox4.Text.Trim());
+                    cmd.Parameters.AddWithValue("@current_stock", TextBox4.Text.Trim());
+                    cmd.Parameters.AddWithValue("@book_img_link", filepath);
+                    cmd.Parameters.AddWithValue("@description", TextBox6.Text.Trim());
+
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    GridView1.DataBind();
+                    Response.Write("<script>alert('Infos Livre modifiées!');</script>");
+
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('ID Invalide');</script>");
+            }
+        }
+        void deleteBookByID()
+        {
+            if (checkIfBookExists())
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    SqlCommand cmd = new SqlCommand("DELETE from book_master_tbl WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("<script>alert('Livre supprimé!');</script>");
+
+                    GridView1.DataBind();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid Member ID');</script>");
+            }
+        }
         void fillAuthorCategoryValues()
         {
             try
